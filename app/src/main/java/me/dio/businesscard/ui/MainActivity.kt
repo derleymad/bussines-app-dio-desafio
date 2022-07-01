47cf3a8f92936d67cc495be5eed6f2b7c7a440f6
derleymad <70535956+derleymad@users.noreply.github.com>
2022-07-01 17:26:53 -0300

    perfect theme finished

--- app/src/main/java/me/dio/businesscard/ui/MainActivity.kt
+++ app/src/main/java/me/dio/businesscard/ui/MainActivity.kt
@@ -1,16 +1,22 @@
 package me.dio.businesscard.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import me.dio.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        insert()
+}
+    private fun insert(){
+        binding.fab.setOnClickListener{
+            val intent = Intent(this@MainActivity,AddBusinessCardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
